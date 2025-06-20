f736d3a7ad0620d7a9cd2b2e38038e19836b86a0
Murilo Luis Calvo Neves <muriloluiscalvoneves2004@hotmail.com>
2025-06-20 00:54:21 -0300

    Update parcial bateria de testes

--- bateria_de_testes.py
+++ bateria_de_testes.py
@@ -4,71 +4,59 @@ import matplotlib.pyplot as plt
 
 algoritmo_cruzamento = ["0", "1"]
 
-numero_individuos = ["50", "100"]
+numero_individuos = ["50"]
 
-chances_mutacao = ["0.005", "0.01", "0.1"]
+chances_mutacao = ["0.01"]
 
-criterio_parada = ["100", "500", "1000"]
+criterio_parada = ["500"]
 
+numero_threads = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+# Aumentar p/ instâncias dps que estiver funcionando
 arquivos_de_teste = [
                      "u574.tsp",
                      "kroA100.tsp",
                      "a280.tsp"
 ]
 
-timestamps_gerados = []  # Arquivos .txt gerados e salvos
+# timestamps_gerados = []  # Arquivos .txt gerados e salvos
 
-resultados_obtidos = []  # OBS: Guardar tuplas (custo, tempo), na mesma ordem do timestamps_gerados
+# resultados_obtidos = []  # OBS: Guardar tuplas (custo, tempo), na mesma ordem do timestamps_gerados
 
 if __name__ == "__main__":
     
     # executavel.exe [instancia] [operador_crz] [tamanho_populacao] [chance_mutacao] [criterio_parada]
     
-    num_total_testes = len(algoritmo_cruzamento) * len(numero_individuos) * len(chances_mutacao) * len(criterio_parada) * len(arquivos_de_teste)
+    num_total_testes = len(algoritmo_cruzamento) * len(numero_individuos) * len(chances_mutacao) * len(criterio_parada) * len(arquivos_de_teste) * len(numero_threads)
 
     num_executado_de_testes = 0
 
+    # Testes com threads
     for operador_crz in algoritmo_cruzamento:
         for tam_pop in numero_individuos:
             for chance_mut in chances_mutacao:
                 for qtd_parada in criterio_parada:                    
                     for instancia in arquivos_de_teste:
-                        
-                        str_nome_teste = f"{operador_crz}-{tam_pop}-{chance_mut.replace(".","")}-{qtd_parada}-{instancia.replace(".tsp", "")}"
+                        for num_threads in numero_threads:    
+                            # str_nome_teste = f"{operador_crz}-{tam_pop}-{chance_mut.replace(".","")}-{qtd_parada}-{instancia.replace(".tsp", "")}"
+                            temp = instancia.replace(".tsp", "")
+                            str_nome_teste = f"{temp}-{num_threads}"
 
-                        # Aqui dá pra fazer um igualzinho pra linux e só trocar na hora de executar mesmo
-                        str_execucao_windows = f"tsp.exe instancias/{instancia} {operador_crz} {tam_pop} {chance_mut} {qtd_parada}"
-                        print(f"Realizando teste com os seguintes parametros: {str_execucao_windows}")
-                        
-                        # Verifica se existe já um timestamp para essa execução, se sim, pula
-                        if os.path.isfile(f"timestamps/{str_nome_teste}.txt"):
-                            continue
+                            str_execucao_windows = f"tsp.exe instancias/{instancia} {operador_crz} {tam_pop} {chance_mut} {qtd_parada} {num_threads}"
+                            print(f"Realizando teste com os seguintes parametros: {str_execucao_windows}")
+                            
+                            if os.path.isfile(f"timestamps/{str_nome_teste}.txt"):
+                                continue
 
-                        os.system(str_execucao_windows)
-                        
-                        # Abrir o timestamp e pegar a última linha (tempo e custo)
-                                      
-                        shutil.move('timestamp.txt', f'timestamps/{str_nome_teste}.txt')
+                            os.system(str_execucao_windows)
+                                        
+                            shutil.move('timestamp.txt', f'timestamps/{str_nome_teste}.txt')
+                            shutil.move('tempo_construcao.txt', f'timestamps/constr-{str_nome_teste}.txt')
 
-                        timestamps_gerados.append(f"{str_nome_teste}.txt")  
+                            num_executado_de_testes += 1
+                            print(f"Teste numero {num_executado_de_testes} de {num_total_testes} feito [{round((num_executado_de_testes*100)/num_total_testes, 2)}%]")
 
-                        # valores = (tempo, custo)
-                        # resultados_obtidos.append(valores)
+    # TODO: Alterar o código sequencial p/ gerar o arquivo de construção tbm
 
-                        num_executado_de_testes += 1
-                        print(f"Teste numero {num_executado_de_testes} de {num_total_testes} feito [{round((num_executado_de_testes*100)/num_total_testes, 2)}%]")
-
-    # for instancia in arquivos_de_teste:
-        # instancia = instancia.replace(".tsp", "")
-
-        # gerar_graficos(instancia)
-
-    # for variacao in timestamps_gerados:
-    #     variacao = "".join(variacao.split("-")[0:-1])
-
-    #     print(f"Gerando tabela para variacao: {variacao}")
-
-    #     if os.path.isfile(f"tabelas/{variacao}.txt"):
-    #         continue
-
-    #     gerar_tabelas(variacao)
+    # Repetir os testes com o código sequencial e colocar na pasta tbm
+    
